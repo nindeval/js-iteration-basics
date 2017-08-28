f8633495c9ba65fd7389a73460d0191d474b0144
travis <t3patterson@gmail.com>
2017-08-28 09:24:46 -0500

    creating exercises

---
+++ ex-07-getFirstLetters.js
@@ -0,0 +1,28 @@
+
+/**
+ * getFirstLetters()
+ *
+ * Write a function called `getFirstLetters` that takes an array of strings
+ * and returns another array of strings with only the first letter
+ *
+ * Hint: you may want to invoke the `reverseString` function you wrote earlier
+ * from the previous queestion
+*/
+
+
+
+
+
+
+//*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*~*~*~*~
+let outputArray1 = getFirstLetters(['wow', 'milk', 'howdy'])
+console.assert( getFirstLetters(outputArray1[0] === 'w')
+console.assert( getFirstLetters(outputArray1[1] === 'm')
+console.assert( getFirstLetters(outputArray1[2] === 'h')
+
+
+let outputArray2 = getFirstLetters(['kayak', 'chair', 'jacket', 'nice!'])
+console.assert( getFirstLetters(outputArray2[0] === 'k')
+console.assert( getFirstLetters(outputArray2[1] === 'c')
+console.assert( getFirstLetters(outputArray2[2] === 'j')
+console.assert( getFirstLetters(outputArray2[3] === 'n')
